7dc71df9d95c26faf377ee6b86e45b08527fd177
SAINOOMHSENG <iitarkar92@gmail.com>
2026-09-23 17:39:47 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 window.HOUSEHOLD_FIREBASE = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyBWWHNQMsTEQW5H5sFAkp76k0TiShKkFBY",
   authDomain: "household-822be.firebaseapp.com",
   projectId: "household-822be",
   storageBucket: "household-822be.firebasestorage.app",
   messagingSenderId: "306642751835",
-  appId: "1:306642751835:web:202ac..."
+  appId: "1:306642751835:web:202ac77189e3e68ea28a7e"
 };
 
 window.HOUSEHOLD_MAILER = {
-  url: "https://script.google.com/macros/s/AKfycb.../exec",
+  url: "PASTE_YOUR_SCRIPT_URL_HERE",
   to: "financial.report@gmail.com",
 };
